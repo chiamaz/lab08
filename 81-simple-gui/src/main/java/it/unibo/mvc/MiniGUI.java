d53d0259edd2db50ca2bf6802bc50337636767b5
Chiara Mazzoni <chiara.mazzoni12@studio.unibo.it>
2025-11-11 15:32:29 +0100

    Ex.81, part1 finished

--- 81-simple-gui/src/main/java/it/unibo/mvc/MiniGUI.java
+++ 81-simple-gui/src/main/java/it/unibo/mvc/MiniGUI.java
@@ -1,5 +1,6 @@
 package it.unibo.mvc;
 
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -32,9 +33,18 @@ public class MiniGUI {
         final JPanel canvas = new JPanel();
         canvas.setLayout(new BorderLayout());
         final JButton write = new JButton("Print a random number on standard output");
-        canvas.add(write, BorderLayout.CENTER);
+        //canvas.add(write, BorderLayout.CENTER);
         frame.setContentPane(canvas);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        /*
+         * new jPanel
+         */
+        final JPanel panel = new JPanel();
+        panel.setLayout(new BoxLayout(panel, BoxLayout.X_AXIS));
+        canvas.add(panel, BorderLayout.CENTER);
+        frame.setContentPane(panel);
+        panel.add(write);
         /*
          * Handlers
          */
@@ -69,11 +79,14 @@ public class MiniGUI {
         /*
          * Resize the frame to minimum size
          */
-        frame.pack();
+        frame.setMinimumSize(frame.getMinimumSize());
+
+        //frame.pack();
         /*
          * OK, ready to pull the frame onscreen
          */
         frame.setVisible(true);
+
     }
 
     /**
