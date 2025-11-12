aa09236ada430a7d21003a4d169055c0ad27c788
Chiara Mazzoni <chiara.mazzoni12@studio.unibo.it>
2025-11-12 11:10:57 +0100

    Ex. 82: part 1 and part 2 finished

--- 82-tdd-deathnote/src/test/java/it/unibo/deathnote/TestDeathNote.java
+++ 82-tdd-deathnote/src/test/java/it/unibo/deathnote/TestDeathNote.java
@@ -1,5 +1,23 @@
 package it.unibo.deathnote;
 
+import org.junit.jupiter.api.BeforeEach;
+
+import it.unibo.deathnote.api.DeathNote;
+
 class TestDeathNote {
+    private String name;
+    private String cause;
+    private String details;
+    private int ruleNumber;
+
+    @BeforeEach
+    void setUp(){
+        this.name = new String("mRossi");
+        this.cause = new String("shot");
+        this.details = new String("lot of blood");
+        this.ruleNumber = 8;
+    }
+
+    
 
 }
