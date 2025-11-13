79055d59ff305604f990efdeafaa5b0a04519c66
Chiara Mazzoni <chiara.mazzoni12@studio.unibo.it>
2025-11-13 13:30:58 +0100

    Ex. 82; part 3, point 4 done

--- 82-tdd-deathnote/src/test/java/it/unibo/deathnote/TestDeathNote.java
+++ 82-tdd-deathnote/src/test/java/it/unibo/deathnote/TestDeathNote.java
@@ -16,8 +16,11 @@ import it.unibo.deathnote.impl.ImplDeathNote;
 
 class TestDeathNote {
     private static final int RULE = 3;
+    private static final String CAUSE1 = "shot";
+    private static final String CAUSE2 = "karting accident";
+    private static final String CAUSE_DEF = "heart attack";
     private static final String NAME1 = "Mario";  
-    private static final String NAME2 = "Mario";  
+    private static final String NAME2 = "Jessica";  
     private DeathNote deathNote;
 
     @BeforeEach
@@ -55,11 +58,36 @@ class TestDeathNote {
         assertEquals(deathNote.isNameWritten(NAME1), true);
         assertEquals(deathNote.isNameWritten(NAME2), false);
         assertEquals(deathNote.isNameWritten(" "), false);
-
-
     }
 
 
-    
+    @Test
+    public void testCauseOfDeath(){
+
+        assertThrows(IllegalStateException.class, new Executable() {
+
+            @Override
+            public void execute() throws Throwable {
+                deathNote.writeDeathCause(CAUSE1);
+            }
+            
+        });
+
+        deathNote.writeName(NAME1);
+        assertEquals(deathNote.getDeathCause(NAME1), CAUSE_DEF);
+
+        deathNote.writeName(NAME2);
+        //deathNote.writeDeathCause(CAUSE2);
+        assertEquals(deathNote.writeDeathCause(CAUSE2), true);
+        assertEquals(deathNote.getDeathCause(NAME2), CAUSE2);
+        //RICONTROLLA
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+           throw new IllegalArgumentException();
+        }
+        deathNote.writeDeathCause(CAUSE1);
+        assertEquals(deathNote.getDeathCause(NAME2), CAUSE2);
+    }
 
 }
