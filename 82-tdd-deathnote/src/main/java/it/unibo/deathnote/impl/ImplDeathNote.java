aafd4eae514a98de2ce30da0db0522ea1c9bc092
Chiara Mazzoni <chiara.mazzoni12@studio.unibo.it>
2025-11-13 15:58:53 +0100

    Ex.82: finished part 6

--- 82-tdd-deathnote/src/main/java/it/unibo/deathnote/impl/ImplDeathNote.java
+++ 82-tdd-deathnote/src/main/java/it/unibo/deathnote/impl/ImplDeathNote.java
@@ -1,49 +1,105 @@
 package it.unibo.deathnote.impl;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import it.unibo.deathnote.api.DeathNote;
 
 public class ImplDeathNote implements DeathNote{
+    private static final long MSEC1 = 40;
+    private static final long MSEC2 = 6400;
+    private static final String CAUSE_DEF = "heart attack";
+
+    private Map<String, Victim> map = new HashMap<>();
+    private long time1;
+    private long time2;
+    private String lastName;
 
     @Override
     public String getRule(int ruleNumber) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getRule'");
+        if(ruleNumber < 1 || ruleNumber > RULES.size()){
+            throw new IllegalArgumentException("the given rule number is smaller than 1 or larger than the number of rules");
+        }
+        return RULES.get(ruleNumber);
     }
-
+        
     @Override
     public void writeName(String name) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'writeName'");
+        if (name == null){
+            throw new NullPointerException("the given name is null");
+        }
+        map.put(name, null);
+        this.lastName = name;
+        time1 = System.currentTimeMillis();
     }
 
     @Override
     public boolean writeDeathCause(String cause) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'writeDeathCause'");
+        if (cause == null || map.isEmpty()){
+            throw new IllegalStateException("the given cause is null or there is no name written");
+        }
+             
+        time2 = System.currentTimeMillis();
+
+        if(time2 - time1 <= MSEC1){
+            map.get(lastName).setCause(cause);
+            //forse lastname=null
+            return true;
+        }
+        else{
+            return false;
+        }
     }
 
     @Override
     public boolean writeDetails(String details) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'writeDetails'");
+        if (details == null || map.isEmpty()){
+            throw new IllegalStateException("the given details is null or there is no name written");
+        }
+             
+        time2 = System.currentTimeMillis();
+        
+        if(time2 - time1 <= MSEC2){
+            map.get(lastName).setDetails(details);
+            return true;
+        }
+        else{
+            return false;
+        }    
     }
 
     @Override
     public String getDeathCause(String name) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getDeathCause'");
+        if(this.isNameWritten(name) == false){
+            throw new IllegalArgumentException("the name is not in the deathnote");
+        }
+        final String cause = map.get(name).getCause();
+        if(cause == null){
+            return CAUSE_DEF;
+        }
+        else{
+            return cause;
+        }
+
     }
 
     @Override
     public String getDeathDetails(String name) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getDeathDetails'");
+        if(this.isNameWritten(name) == false){
+            throw new IllegalArgumentException("the name is not in the deathnote");
+        }
+        final String detail = map.get(name).getDetails();
+        if(detail == null){
+            return " ";
+        }
+        else{
+            return detail;
+        }
     }
 
     @Override
     public boolean isNameWritten(String name) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'isNameWritten'");
+        return map.containsKey(name);
     }
 
 
