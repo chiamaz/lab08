ec4c5cdd969e176b8b555801a81ea4e97b41f13e
Chiara Mazzoni <chiara.mazzoni12@studio.unibo.it>
2025-11-15 15:56:50 +0100

    Ex 83: Modify the architecture in such a way that multiple views are supported at the same time.

--- 83-mvc-gui-reflection/src/main/java/it/unibo/mvc/controller/DrawNumberControllerImpl.java
+++ 83-mvc-gui-reflection/src/main/java/it/unibo/mvc/controller/DrawNumberControllerImpl.java
@@ -3,7 +3,10 @@ package it.unibo.mvc.controller;
 import it.unibo.mvc.api.DrawNumber;
 import it.unibo.mvc.api.DrawNumberController;
 import it.unibo.mvc.api.DrawNumberView;
+import it.unibo.mvc.api.DrawResult;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 /**
@@ -13,7 +16,9 @@ import java.util.Objects;
 public final class DrawNumberControllerImpl implements DrawNumberController {
 
     private final DrawNumber model;
-    private DrawNumberView view;
+    //private DrawNumberView view;
+    //modifica per avere più view
+    private List<DrawNumberView> views;
 
     /**
      * Builds a new game controller provided a game model.
@@ -22,22 +27,23 @@ public final class DrawNumberControllerImpl implements DrawNumberController {
      */
     public DrawNumberControllerImpl(final DrawNumber model) {
         this.model = model;
+        this.views = new ArrayList<>();
     }
 
     @Override
     public void addView(final DrawNumberView view) {
         Objects.requireNonNull(view, "Cannot set a null view");
-        if (this.view != null) {
-            throw new IllegalStateException("The view is already set! Multiple views are not supported");
-        }
-        this.view = view;
+        this.views.add(view);
         view.setController(this);
         view.start();
     }
 
     @Override
     public void newAttempt(final int n) {
-        Objects.requireNonNull(view, "There is no view attached!").result(model.attempt(n));
+        final DrawResult result = this.model.attempt(n);
+        for (DrawNumberView view : this.views) {
+            Objects.requireNonNull(view, "There is no view attached!").result(result);
+        }
     }
 
     @Override
